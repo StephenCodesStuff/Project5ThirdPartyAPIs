a08daaf05a7f7a83da38e744d69396ec4df38987
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-05-24 21:39:00 -0500

    local storage work

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function () {
 
  var currentDay = $('#currentDay')
  
  var saveButton = $('.saveBtn')
  var rowEl = $('.row')
+ var textBoxInput = $('.description')
  console.log(saveButton)
 //  console.log(currentTime)
 
@@ -21,20 +20,16 @@ $(document).ready(function () {
   // useful when saving the description in local storage?
 // saveButton.on('click', saveBoxText)
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  
   var currentHour= dayjs().format('H')
   // var currentHour= 13
   console.log(currentHour)
   
   $('.time-block').each(function() {
-    console.log(this)
+    // console.log(this)
     var timeID = parseInt($(this).attr("id"));
-    console.log(timeID)
+    // console.log(timeID)
     
     if(timeID < currentHour) {
       $(this).addClass("past");
@@ -44,19 +39,6 @@ $(document).ready(function () {
       $(this).addClass("future");
     }
   });
-  // var dayTime = $('.time');
-  // dayTime.each((dayTime))
-
-  
-
-
-  // if (projectDate.isBefore(today)) {
-  //   rowEl.addClass('past');
-  // } else if (projectDate.isSame(today)) {
-  //   rowEl.addClass('present');
-  // } else if (projectDate.isAfter(today)) {
-  //   rowEl.addClass('future')
-  // }
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -71,5 +53,72 @@ $(document).ready(function () {
     currentDay.text(rightNow);
   }
 
+  $('.saveBtn').each(function(){
+    console.log(this)
+
+  })
+  
+  //Create
+  //Find The button will read the element and make a new object.
+
+  //Read 
+  // Find the object in local storage. Parse it out. Find the parent element using the ID (for loop) and the add it to the HTML
+
+  //Update '
+  //The new object will but put into the array
+  //Delete 
+  //The old object will be deleted
+  
+  function findTextFromStorage(){
+    var schdText = localStorage.getItem('schedule');
+    if (schdText) {
+      schdText = JSON.parse(schdText);
+      console.log(schdText)
+    } else {
+      schdText = [];
+    }
+    return schdText;
+  }
+
+  var schdText = findTextFromStorage();
+  
+  // var schdText =JSON.parse(localStorage.getItem("schedule")) || [];
+  console.log(schdText)
+
+  function saveText(event){
+    event.preventDefault();
+    console.log("working");
+    var discriptionText = textBoxInput.val();
+    // console.log(discriptionText);
+    var buttonID = $(this).parent().attr("id");
+    // console.log($(this).parent().attr("id"))
+    // console.log(buttonID);
+    var savedTexts = []
+    var savedText = {
+      boxID: buttonID,
+      text: discriptionText,
+    }
+    console.log(savedText)
+    
+    localStorage.setItem("schedule", JSON.stringify(savedText));
+    // savedTexts.push(savedText)
+    // saveTextToStorage()
+  }
+  saveButton.on('click', saveText);
 
 });
+
+
+
+
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+
+
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
